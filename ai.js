ea36f53c2cc83494739c5554e66fabf3e8044d59
Jokimax <jonaxthedreadlord@gmail.com>
2024-04-02 18:31:22 +0300

    general updates

--- ai.js
+++ ai.js
@@ -1,43 +1,43 @@
 const checkers = require("./checkers")
 
-function getBestMove(position, forcedCaptures, canCaptureBackwards, depth){
-    let potentialMoves = checkers.getMoves(position, "a", forcedCaptures, canCaptureBackwards)
-    if(potentialMoves.length === 1){
+function getBestMove(position, player, forcedCaptures, canCaptureBackwards, depth) {
+    let potentialMoves = checkers.getMoves(position, player, forcedCaptures, canCaptureBackwards)
+    if (potentialMoves.length === 1) {
         return potentialMoves[0]
     }
 
     let bestMove = []
     let highestScore = -1000000
     let lowestScore = 1000000
-    
-    for(let i = 0; i < potentialMoves.length; i++) { 
-        makeMove(position, potentialMoves[i])
-        let score = search(position, depth - 1, highestScore, lowestScore, "e")
-        unmakeMove(position, potentialMoves[i])
-        if(score > highestScore){
-            highestScore = score
-            bestMove = potentialMoves[i]
+
+    for (let move of potentialMoves) {
+        makeMove(position, move)
+        let score = search(position, depth - 1, highestScore, lowestScore, getOpponent(player));
+        unmakeMove(position, move)
+        if (player === "w" && score > highestScore) {
+            highestScore = score;
+            bestMove = move
+        } else if (player === "b" && score < lowestScore) {
+            lowestScore = score
+            bestMove = move
         }
     }
 
-    return bestMove
+    return bestMove;
 
     function search(pos, depth, alpha, beta, player) {
-        let moves
-        if (player === 'a') {
-            moves = checkers.getMoves(pos, 'a', forcedCaptures, canCaptureBackwards)
-            if (moves.length === 0) return -100000
-        } else {
-            moves = checkers.getMoves(pos, 'e', forcedCaptures, canCaptureBackwards)
-            if (moves.length === 0) return 100000
+        let moves = checkers.getMoves(pos, player, forcedCaptures, canCaptureBackwards);
+        if (moves.length === 0) {
+            if (player === "w") return -100000
+            else return 100000
         }
-    
-        if (depth === 0) return countScore(pos, player)
-    
-        if (player === 'a') {
+
+        if (depth === 0) return countScore(pos, player);
+
+        if (player === "w") {
             for (let move of moves) {
                 makeMove(pos, move)
-                let score = search(pos, depth - 1, alpha, beta, 'e')
+                let score = search(pos, depth - 1, alpha, beta, "b")
                 unmakeMove(pos, move)
                 alpha = Math.max(alpha, score)
                 if (alpha >= beta) return alpha
@@ -46,103 +46,38 @@ function getBestMove(position, forcedCaptures, canCaptureBackwards, depth){
         } else {
             for (let move of moves) {
                 makeMove(pos, move)
-                let score = search(pos, depth - 1, alpha, beta, 'a')
+                let score = search(pos, depth - 1, alpha, beta, "w")
                 unmakeMove(pos, move)
                 beta = Math.min(beta, score)
                 if (beta <= alpha) return beta
             }
             return beta
         }
-    }    
+    }
 
-    function countScore(pos){
-        let score = 0
-        for(let y = 0; y < 8; y++){
-            for(let x = 0; x < 8; x++){
-                if(pos[y][x] === "a") score++
-                else if(pos[y][x] === "e") score--
-                else if(pos[y][x] === "A") score += 10
-                else if(pos[y][x] === "E") score -= 10
+    function countScore(pos) {
+        let score = 0;
+        for (let y = 0; y < 8; y++) {
+            for (let x = 0; x < 8; x++) {
+                if (pos[y][x] === "b") score -= 100
+                else if (pos[y][x] === "w") score += 100
+                else if (pos[y][x] === "B") score -= 1000
+                else if (pos[y][x] === "W") score += 1000
             }
         }
         return score
     }
-}
 
-function getBestMoveOpponent(position, forcedCaptures, canCaptureBackwards, depth){
-    let potentialMoves = checkers.getMoves(position, "e", forcedCaptures, canCaptureBackwards)
-    if(potentialMoves.length === 1){
-        return potentialMoves[0]
-    }
-
-    let bestMove = []
-    let highestScore = -1000000
-    let lowestScore = 1000000
-    
-
-    for(let i = 0; i < potentialMoves.length; i++) { 
-        makeMove(position, potentialMoves[i])
-        let score = search(position, depth - 1, highestScore, lowestScore, "a")
-        unmakeMove(position, potentialMoves[i])
-        if(score > highestScore){
-            highestScore = score
-            bestMove = potentialMoves[i]
-        }
-    }
-
-    return bestMove
-
-    function search(pos, depth, alpha, beta, player) {
-        let moves
-        if (player === 'e') {
-            moves = checkers.getMoves(pos, 'e', forcedCaptures, canCaptureBackwards)
-            if (moves.length === 0) return -100000
-        } else {
-            moves = checkers.getMoves(pos, 'a', forcedCaptures, canCaptureBackwards)
-            if (moves.length === 0) return 100000
-        }
-    
-        if (depth === 0) return countScore(pos, player)
-    
-        if (player === 'e') {
-            for (let move of moves) {
-                makeMove(pos, move)
-                let score = search(pos, depth - 1, alpha, beta, 'a')
-                unmakeMove(pos, move)
-                alpha = Math.max(alpha, score)
-                if (alpha >= beta) return alpha
-            }
-            return alpha
-        } else {
-            for (let move of moves) {
-                makeMove(pos, move)
-                let score = search(pos, depth - 1, alpha, beta, 'e')
-                unmakeMove(pos, move)
-                beta = Math.min(beta, score)
-                if (beta <= alpha) return beta
-            }
-            return beta;
-        }
-    }   
-
-    function countScore(pos){
-        let score = 0
-        for(let y = 0; y < 8; y++){
-            for(let x = 0; x < 8; x++){
-                if(pos[y][x] === "a") score--
-                else if(pos[y][x] === "e") score++
-                else if(pos[y][x] === "A") score -= 10
-                else if(pos[y][x] === "E") score += 10
-            }
-        }
-        return score
+    function getOpponent(player) {
+        return player === "b" ? "w" : "b"
     }
 }
+
 
 function makeMove(pos, move) {
     let lastElementIndex = move.length - 1
-    if(move[lastElementIndex]["y"] === 7 && move[0]["originalPiece"] === "a") pos[move[lastElementIndex]["y"]][move[lastElementIndex]["x"]] = "A"
-    else if(move[lastElementIndex]["y"] === 0 && move[0]["originalPiece"] === "e") pos[move[lastElementIndex]["y"]][move[lastElementIndex]["x"]] = "E"
+    if(move[lastElementIndex]["y"] === 7 && move[0]["originalPiece"] === "b") pos[move[lastElementIndex]["y"]][move[lastElementIndex]["x"]] = "B"
+    else if(move[lastElementIndex]["y"] === 0 && move[0]["originalPiece"] === "w") pos[move[lastElementIndex]["y"]][move[lastElementIndex]["x"]] = "W"
     else pos[move[lastElementIndex]["y"]][move[lastElementIndex]["x"]] = move[0]["originalPiece"]
     for(let i = 0; i < lastElementIndex; i++){
         pos[move[i]["y"]][move[i]["x"]] = "*"
@@ -155,4 +90,4 @@ function unmakeMove(pos, move) {
     }
 }
 
-module.exports = { getBestMove, getBestMoveOpponent,  }
+module.exports = { getBestMove }
--- checkers.js
+++ checkers.js
@@ -4,14 +4,14 @@ function getMoves(position, pieceType, forcedCaptures, canCaptureBackwards) {
 
     for (let y = 0; y < 8; y++) {
         for (let x = 0; x < 8; x++) {
-            if ((pieceType === 'a' && position[y][x] === 'a') || (pieceType === 'e' && position[y][x] === 'e')) {
+            if ((pieceType === "b" && position[y][x] === "b") || (pieceType === "w" && position[y][x] === "w")) {
                 const res = getPeasantMoves(position, x, y, pieceType, hasToCapture, forcedCaptures, canCaptureBackwards)
                 if (res.hasToCapture && !hasToCapture) {
                     moves = []
                     hasToCapture = true
                 }
                 moves = moves.concat(res.moves);
-            } else if ((pieceType === 'a' && position[y][x] === 'A') || (pieceType === 'e' && position[y][x] === 'E')) {
+            } else if ((pieceType === "b" && position[y][x] === "B") || (pieceType === "w" && position[y][x] === "W")) {
                 const res = getKingMoves(position, x, y, pieceType, hasToCapture, forcedCaptures)
                 if (res.hasToCapture && !hasToCapture) {
                     moves = []
@@ -36,7 +36,7 @@ function getPeasantMoves(position, x, y, pieceType, hasToCapture, forcedCaptures
     }
 
     if (!hasToCapture) {
-        let forwardY = pieceType === 'a' ? y + 1 : y - 1
+        let forwardY = pieceType === "b" ? y + 1 : y - 1
         if (x - 1 >= 0 && x - 1 < 8 && forwardY >= 0 && forwardY < 8 && position[forwardY][x - 1] === "*") {
             moves.push([{"x": x, "y": y, "originalPiece": originalPiece}, {"x": x - 1, "y": forwardY, "originalPiece": "*"}])
         }
@@ -52,7 +52,7 @@ function getPeasantMoves(position, x, y, pieceType, hasToCapture, forcedCaptures
     function getPieceCaptures(pos, x1, y1, pieceType, capture, canCaptureBackwards) {
         let capturesRes = []
         let directions = [-1, 1]
-        let backwardDirection = canCaptureBackwards ? [-1, 1] : [pieceType === 'a' ? 1 : -1]
+        let backwardDirection = canCaptureBackwards ? [-1, 1] : [pieceType === "b" ? 1 : -1]
 
         for (let dy of backwardDirection) {
             for (let dx of directions) {
@@ -62,8 +62,8 @@ function getPeasantMoves(position, x, y, pieceType, hasToCapture, forcedCaptures
                 let enemyY = y1 + dy
 
                 if (nextX >= 0 && nextX < 8 && nextY >= 0 && nextY < 8 &&
-                    ((pieceType === 'a' && (pos[enemyY][enemyX] === "e" || pos[enemyY][enemyX] === "E")) ||
-                    (pieceType === 'e' && (pos[enemyY][enemyX] === "a" || pos[enemyY][enemyX] === "A"))) &&
+                    ((pieceType === "b" && (pos[enemyY][enemyX] === "w" || pos[enemyY][enemyX] === "W")) ||
+                    (pieceType === "w" && (pos[enemyY][enemyX] === "b" || pos[enemyY][enemyX] === "B"))) &&
                     pos[nextY][nextX] === "*") {
                     let capturedPiece = pos[enemyY][enemyX]
                     pos[enemyY][enemyX] = "*"
@@ -130,8 +130,8 @@ function getKingMoves(position, x, y, pieceType, hasToCapture, forcedCaptures) {
             let y = y1 + direction.dy
     
             while (x >= 0 && x < 8 && y >= 0 && y < 8) {
-                if ((pieceType === 'a' && (pos[y][x] === "e" || pos[y][x] === "E")) ||
-                    (pieceType === 'e' && (pos[y][x] === "a" || pos[y][x] === "A"))) {
+                if ((pieceType === "b" && (pos[y][x] === "w" || pos[y][x] === "W")) ||
+                    (pieceType === "w" && (pos[y][x] === "b" || pos[y][x] === "B"))) {
                     let capturedPiece = pos[y][x]
                     let nx = x + direction.dx
                     let ny = y + direction.dy
